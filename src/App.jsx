b5f3f89d38c9d3420b0998b44c865861c2a9838e
Technology-maker <ys5401519@gmail.com>
2025-08-07 09:29:38 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from './components/Home';
-import Login from './components/Login';
+import React from 'react'
+import { Navigate, Route, Routes } from "react-router"
+import Home from './components/home';
+import Login from './components/Login.jsx';
 import Signup from './components/Signup';
-import { useAuth } from './context/AuthProvider';
-
+import { useAuth } from './context/AuthProvider.jsx';
 
 const App = () => {
 
