0c8f835cc6254b8dc75004a88e26fe02783d8831
Technology-maker <ys5401519@gmail.com>
2025-08-22 11:39:30 +0530

    addd ClickSpark

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Prompt from './Prompt';
+import ClickSpark from './snipet/ClickSpark';
 
 const Home = () => {
   const [prompt, setPrompt] = useState([]);
   return (
     <div className="flex h-screen bg-[#1e1e1e] text-white overflow-hidden">
-      {/* Only one Sidebar */}
-      <Sidebar setPrompt={setPrompt} />
 
-      {/* Prompt area */}
-      <div className="flex-1 flex flex-col w-full">
-        <div className="flex-1 flex items-center justify-center">
-          <Prompt prompt={prompt} setPrompt={setPrompt} />
+      <ClickSpark
+        sparkColor='#fff'
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+
+        {/* Only one Sidebar */}
+        <Sidebar setPrompt={setPrompt} />
+
+        {/* Prompt area */}
+        <div className="flex-1 flex flex-col w-full">
+          <div className="flex-1 flex items-center justify-center">
+            <Prompt prompt={prompt} setPrompt={setPrompt} />
+          </div>
         </div>
-      </div>
+
+      </ClickSpark>
     </div>
   );
 };
