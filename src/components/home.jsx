3d753d0c7767895c97e241bd24eb5c47fa6d1b60
Technology-maker <ys5401519@gmail.com>
2025-08-22 11:34:02 +0530

    adding <SplashCursor />

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Prompt from './Prompt';
+import SplashCursor from './snipet/SplashCursor'
 
 const Home = () => {
   const [prompt, setPrompt] = useState([]);
   return (
     <div className="flex h-screen bg-[#1e1e1e] text-white overflow-hidden">
+      <SplashCursor />
       {/* Only one Sidebar */}
       <Sidebar setPrompt={setPrompt} />
 
