b17cfd8fd0b076e71e8b5d2fcf435d19661e42ff
Technology-maker <ys5401519@gmail.com>
2025-09-16 21:19:31 +0530

    some updating

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,25 +8,16 @@ const Home = () => {
   return (
     <div className="flex h-screen bg-[#1e1e1e] text-white overflow-hidden">
 
-      {/* <ClickSpark
-        sparkColor='#fff'
-        sparkSize={10}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={400}
-      > */}
+      {/* Only one Sidebar */}
+      <Sidebar setPrompt={setPrompt} />
 
-        {/* Only one Sidebar */}
-        <Sidebar setPrompt={setPrompt} />
-
-        {/* Prompt area */}
-        <div className="flex-1 flex flex-col w-full">
-          <div className="flex-1 flex items-center justify-center">
-            <Prompt prompt={prompt} setPrompt={setPrompt} />
-          </div>
+      {/* Prompt area */}
+      <div className="flex-1 flex flex-col w-full">
+        <div className="flex-1 flex items-center justify-center">
+          <Prompt prompt={prompt} setPrompt={setPrompt} />
         </div>
+      </div>
 
-      {/* </ClickSpark> */}
     </div>
   );
 };
